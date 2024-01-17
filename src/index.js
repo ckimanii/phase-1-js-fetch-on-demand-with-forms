006a24d885f0acc05aa32a86c27ca468cf744b08
Kimani <alex.kimani@student.moringaschool.com>
2024-01-17 10:12:49 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const formInput = document.querySelector('#searchByID').value;
+        if (formInput){
+            fetch(`http://localhost:3000/movies/${formInput}`)
+            .then(res => res.json())
+            .then(data => {
+                const title = document.querySelector('section#movieDetails h4');
+                const summary = document.querySelector('section#movieDetails p');
+                title.innerHTML = data.title;
+                summary.innerHTML = data.summary;
+            });
+        };
+    });
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
